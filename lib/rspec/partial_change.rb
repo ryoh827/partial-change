b80970fd9956782604ad8cfdf58de50ad33f531a
Ryoh827 <rn.gjt25@gmail.com>
2024-08-15 23:44:52 +0900

    add partial_change implementation

--- lib/rspec/partial_change.rb
+++ lib/rspec/partial_change.rb
@@ -3,8 +3,66 @@
 require_relative "partial_change/version"
 
 module Rspec
+  # This RSpec matcher partial_change is designed to test if specific keys in a hash-like object have changed as expected.
+  # It supports from and to chaining for asserting the exact values before and after the change,
+  # but does not support usage with not_to.
   module PartialChange
-    class Error < StandardError; end
-    # Your code goes here...
+    extend RSpec::Matchers::DSL
+
+    RSpec::Matchers.define :partial_change do |object, changed_keys|
+      supports_block_expectations
+
+      match do |block|
+        @before = object.dup
+        block.call
+        @after = object
+
+        @from_subset = @before.slice(*changed_keys)
+        @to_subset = @after.slice(*changed_keys)
+
+        unchanged_keys = @before.keys - changed_keys
+
+        # Check that unchanged keys have not been modified
+        unchanged_keys_still_same = unchanged_keys.all? { |key| @before[key] == @after[key] }
+
+        # Check that the specified "from" subset matches the before state
+        from_matches = @from.nil? || @from_subset == @from
+
+        # Check that the specified "to" subset matches the after state
+        to_matches = @to.nil? || @to_subset == @to
+
+        unchanged_keys_still_same && from_matches && to_matches
+      end
+
+      match_when_negated do |_block|
+        raise NotImplementedError,
+              "The `partial_change` matcher does not support `not_to` usage. Please use it with `to`."
+      end
+
+      chain :from do |before|
+        @from = before
+      end
+
+      chain :to do |after|
+        @to = after
+      end
+
+      failure_message do
+        messages = []
+        messages << "from #{@from}" if @from
+        messages << "to #{@to}" if @to
+        expected_change = messages.empty? ? "for keys #{changed_keys}" : messages.join(" ")
+
+        "expected #{object} to change #{expected_change}, but got #{@from_subset} to #{@to_subset}"
+      end
+
+      failure_message_when_negated do
+        "expected #{object} not to change for keys #{changed_keys}, but changes were detected."
+      end
+
+      def unchanged_keys
+        @before.keys
+      end
+    end
   end
 end
--- spec/rspec/partial_change_spec.rb
+++ spec/rspec/partial_change_spec.rb
@@ -1,11 +1,63 @@
 # frozen_string_literal: true
 
-RSpec.describe Rspec::PartialChange do
-  it "has a version number" do
-    expect(Rspec::PartialChange::VERSION).not_to be nil
+require "rspec/partial_change"
+
+RSpec.describe "partial_change matcher" do
+  let(:object) { { a: 1, b: 2, c: 3 } }
+
+  it "matches when specified keys change as expected" do
+    expect do
+      object[:b] = 99
+    end.to partial_change(object, [:b]).from(b: 2).to(b: 99)
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "matches when only changed keys are specified and no from/to is provided" do
+    expect do
+      object[:b] = 99
+    end.to partial_change(object, [:b])
+  end
+
+  it "matches when multiple keys change as expected" do
+    expect do
+      object[:a] = 10
+      object[:b] = 99
+    end.to partial_change(object, %i[a b]).from(a: 1, b: 2).to(a: 10, b: 99)
+  end
+
+  it "raises an error when used with `not_to`" do
+    expect do
+      expect do
+        object[:b] = 99
+      end.not_to partial_change(object, [:b])
+    end.to raise_error(NotImplementedError,
+                       "The `partial_change` matcher does not support `not_to` usage. Please use it with `to`.")
+  end
+
+  it "fails when the 'from' value does not match the actual initial value" do
+    expect do
+      expect do
+        object[:b] = 99
+      end.to partial_change(object, [:b]).from(b: 3).to(b: 99)
+    end.to raise_error(RSpec::Expectations::ExpectationNotMetError,
+                       /expected \{:a=>1, :b=>99, :c=>3\} to change from \{:b=>3\} to \{:b=>99\}, but got \{:b=>2\} to \{:b=>99\}/)
+  end
+
+  it "fails when the 'to' value does not match the actual final value" do
+    expect do
+      expect do
+        object[:b] = 98
+      end.to partial_change(object, [:b]).from(b: 2).to(b: 99)
+    end.to raise_error(RSpec::Expectations::ExpectationNotMetError,
+                       /expected \{:a=>1, :b=>98, :c=>3\} to change from \{:b=>2\} to \{:b=>99\}, but got \{:b=>2\} to \{:b=>98\}/)
+  end
+
+  it "fails when an unchanged key is modified" do
+    expect do
+      expect do
+        object[:a] = 100
+        object[:b] = 99
+      end.to partial_change(object, [:b])
+    end.to raise_error(RSpec::Expectations::ExpectationNotMetError,
+                       /expected \{:a=>100, :b=>99, :c=>3\} to change for keys \[:b\], but got \{:b=>2\} to \{:b=>99\}/)
   end
 end
